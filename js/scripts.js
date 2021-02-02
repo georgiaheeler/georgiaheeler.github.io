fd62c2a6ae018bd52aebdacc8f2ed992e23e2c6f
Georgia <georgia.uk95@gmail.com>
2021-02-02 18:23:41 +0000

    Create JS
    - Add new hamburger menu toggle
    - Add smooth scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,40 @@
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
 
+// Repsonsive hamburger menu
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+  x.className += ' responsive';
+} else {
+  x.className = 'navtoggle';
+}
+}
 
-console.log("Hi there, welcome to my portfolio site!")
+// Scroll to top button
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement
+
+function handleScroll() {
+  // Do something on scroll - 0.15 is the percentage the page has to scroll before the button appears
+  // This can be changed - experiment
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
+  if ((rootElement.scrollTop / scrollTotal ) > 0.9) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    // Hide button
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
