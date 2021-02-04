e45d969dfaac75b4a3fea0e610cd043be4f31d76
Georgia <georgia.uk95@gmail.com>
2021-02-04 18:40:20 +0000

    update scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-<script src="js/scripts.js"></script>
-
 // Repsonsive hamburger menu
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
@@ -10,6 +7,8 @@ function menuToggle() {
   x.className = 'navtoggle';
 }
 }
+
+
 
 // Scroll to top button
 var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
