f5ee1e038e19da799e52a340a8f5781812c3c7bf
Georgia <georgia.uk95@gmail.com>
2021-02-14 17:56:51 +0000

    Fix errors from linter

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 // Repsonsive hamburger menu
-function menuToggle() {
+function menuToggle () {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
     x.className += ' responsive';
@@ -9,29 +9,29 @@ function menuToggle() {
 }
 
 // Scroll to top button
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var scrollToTopBtn = document.querySelector('.scrollToTopBtn')
 var rootElement = document.documentElement
 
-function handleScroll() {
+function handleScroll () {
   // Do something on scroll - 0.15 is the percentage the page has to scroll before the button appears
   // This can be changed - experiment
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
   if ((rootElement.scrollTop / scrollTotal) > 0.9) {
     // Show button
-    scrollToTopBtn.classList.add("showBtn")
+    scrollToTopBtn.classList.add('showBtn')
   } else {
     // Hide button
-    scrollToTopBtn.classList.remove("showBtn")
+    scrollToTopBtn.classList.remove('showBtn')
   }
 }
 
-function scrollToTop() {
+function scrollToTop () {
   // Scroll to top logic
   rootElement.scrollTo({
     top: 0,
-    behavior: "smooth"
+    behavior: 'smooth'
   })
 }
 
-scrollToTopBtn.addEventListener("click", scrollToTop)
-document.addEventListener("scroll", handleScroll)
+scrollToTopBtn.addEventListener('click', scrollToTop)
+document.addEventListener('scroll', handleScroll)
