3ceaeded216b36a358c2ff36a77b66f6dcee24e3
Anil Kumar <anilbms75@gmail.com>
2021-02-18 20:14:15 +0100

    Working js solution for scroll top on project pages

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,23 @@ function handleScroll () {
   // Do something on scroll - 0.15 is the percentage the page has to scroll before the button appears
   // This can be changed - experiment
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-  if ((rootElement.scrollTop / scrollTotal) > 0.9) {
-    // Show button
-    scrollToTopBtn.classList.add('showBtn')
+
+  if (document.querySelector('#knitcase') || document.querySelector('#quest')) {
+    if (rootElement.scrollTop > 500) {
+      // Show button
+      scrollToTopBtn.classList.add("showBtn")
+    } else {
+      // Hide button
+      scrollToTopBtn.classList.remove("showBtn")
+    }
   } else {
-    // Hide button
-    scrollToTopBtn.classList.remove('showBtn')
+    if ((rootElement.scrollTop / scrollTotal ) > 0.9) {
+    // Show button
+    scrollToTopBtn.classList.add("showBtn")
+    } else {
+      // Hide button
+      scrollToTopBtn.classList.remove("showBtn")
+    }
   }
 }
 
